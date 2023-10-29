49abf95e6109210df71a9bd03d13cb71c918d990
muhammadBobur0 <muhammadboburbek@gmail.com>
2023-10-29 19:46:07 +0500

    added

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import { PORT } from './config.js';
 import modules from './modules/index.js';
 import cors from 'cors';
 import fileUpload from 'express-fileupload';
+import path from 'path'
 
 // EXPRESS
 let app = express();
@@ -13,7 +14,7 @@ app.use(express.json());
 app.use(fileUpload());
 app.use(cors());
 app.use(modules);
-app.use(express.static('', 'uploads'));
+app.use(express.static(path.join('uploads')))
 
 // APP LISTEN
 app.listen(PORT, () => console.log('http://localhost:' + PORT));
